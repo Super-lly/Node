440e30ba6c6812d36ff5a22055a6ff44d6085c04
Super-lly <843182286@qq.com>
2022-01-14 16:51:37 +0800

    main

--- node_project/app_server/router_handler/user.js
+++ node_project/app_server/router_handler/user.js
@@ -1,6 +1,8 @@
 // 用户路由处理函数
 // 导入数据库
 const db = require('../db/index')
+// 导入加密模块
+const bcrypt = require('bcryptjs')
 
 // 注册
 register = (req, res) => {
@@ -8,7 +10,7 @@ register = (req, res) => {
   let userinfo = req.body
   // 判断表单数据是否为空
   if (!userinfo.username || !userinfo.password) {
-    return res.send({ status: 1, message: '用户名或密码不正确！' })
+    return res.send({ status: 1, message: '用户名或密码不合法！' })
   } else {
     const sql = 'select * from ev_users where username = ?'
     const insert = 'insert into ev_users set ?'
@@ -20,6 +22,8 @@ register = (req, res) => {
       if (results.length > 0) {
         return res.send({ status: 1, message: '用户已存在' })
       } else {
+        // 对用户密码进行加密存储
+        userinfo.password = bcrypt.hashSync(userinfo.password,10)
         // 若用户不存在 添加用户
         db.query(insert, userinfo, (err, results) => {
           if (err) {
