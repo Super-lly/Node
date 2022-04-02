a8174480043ffdae8250e2d490ae72027fcacdc2
Super-lly <843182286@qq.com>
2022-04-02 10:51:19 +0800

    修改请求地址及其他bug

--- node_project/app_server/app.js
+++ node_project/app_server/app.js
@@ -67,5 +67,5 @@ app.use((err, req, res, next) => {
 
 // 服务器端口
 app.listen(8080, () => {
-  console.log('api server running at http://127.0.0.1:8080');
+  console.log('api server running at http://127.0.0.0:8080');
 })
--- node_project/app_server/db/index.js
+++ node_project/app_server/db/index.js
@@ -3,7 +3,7 @@ const mysql = require('mysql')
 
 // 创建数据库连接
 const db = mysql.createPool({
-  host:'127.0.0.1',
+  host:'47.112.187.95',
   user:'root',
   password:'843182',
   database:'my_db_01'
