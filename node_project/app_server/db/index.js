ab2a3d02df1a2f31c243b20839b4f8a450a9894e
Super-lly <843182286@qq.com>
2022-05-05 18:27:04 +0800

    修改数据库连接地址

--- node_project/app_server/db/index.js
+++ node_project/app_server/db/index.js
@@ -3,7 +3,7 @@ const mysql = require('mysql')
 
 // 创建数据库连接
 const db = mysql.createPool({
-  host:'47.112.187.95',
+  host:'120.25.224.155',
   user:'root',
   password:'843182',
   database:'my_db_01'
